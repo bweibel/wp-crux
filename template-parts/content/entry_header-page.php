f8bfe2a606bb13372345dff78c7ad0647e4ce5c4
Ben Weibel <weibel.b@gmail.com>
2021-07-26 16:55:31 -0700

    Header Style updates

--- template-parts/content/entry_header-page.php
+++ template-parts/content/entry_header-page.php
@@ -7,11 +7,16 @@
 
 namespace WP_Rig\WP_Rig;
 
+$theme = get_template_directory_uri();
+
 ?>
 
 <header class="entry-header">
 	<?php
     get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
-    get_template_part( 'template-parts/content/entry_title', get_post_type() );
-	?>
+    echo '<section class="entry-title-container">';
+        get_template_part( 'template-parts/content/entry_title', get_post_type() );
+        echo file_get_contents($theme . '/assets/images/down_arrow.svg');
+    echo '</section>';
+    ?>
 </header><!-- .entry-header -->
