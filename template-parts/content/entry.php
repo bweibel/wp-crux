fe23b5badaffe81773edf12580a544ce027fb10f
Ben Weibel <weibel.b@gmail.com>
2021-09-07 13:18:27 -0700

    Add single Beer template

--- template-parts/content/entry.php
+++ template-parts/content/entry.php
@@ -28,12 +28,21 @@ namespace WP_Rig\WP_Rig;
 if ( is_singular( get_post_type() ) ) {
 	// Show post navigation only when the post type is 'post' or has an archive.
 	if ( 'post' === get_post_type() || get_post_type_object( get_post_type() )->has_archive ) {
-		the_post_navigation(
-			array(
-				'prev_text' => '<div class="post-navigation-sub"><span>' . esc_html__( 'Previous:', 'wp-rig' ) . '</span></div>%title',
-				'next_text' => '<div class="post-navigation-sub"><span>' . esc_html__( 'Next:', 'wp-rig' ) . '</span></div>%title',
-			)
-		);
+		if( 'crux_beer' === get_post_type() ) {
+			?>
+			<nav class="navigation post-navigation" role="navigation" aria-label="Posts">
+				<a href="/beer" class="button">All Beers</a>
+			</nav>
+			<?php
+		} else {
+			the_post_navigation(
+				array(
+					'prev_text' => '<div class="post-navigation-sub"><span>' . esc_html__( 'Previous:', 'wp-rig' ) . '</span></div>%title',
+					'next_text' => '<div class="post-navigation-sub"><span>' . esc_html__( 'Next:', 'wp-rig' ) . '</span></div>%title',
+				)
+			);
+		}
+
 	}
 
 	// Show comments only when the post type supports it and when comments are open or at least one comment exists.
